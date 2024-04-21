a8889dfbcee39c8db760942e70c6703f9e4f5617
leviathan-7 <m.grober02@yandex.ru>
2024-04-21 15:54:03 +0300

    add some js files

---
+++ WebApplicationGame/Pages/Scene.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationGame.Pages
+{
+    public class Scene : PageModel
+    {
+        public HtmlString OBJtoHTML(string path)
+        {
+            return new HtmlString("<div hidden>\n<img src=\"../Models/" + path + ".png\" id=\"textures_" + path + "\">\n<p id = \"" + path +"\" > " + System.IO.File.ReadAllText("wwwroot/Models/" + path + ".obj") + " </p> \n</div>");
+        }
+    }
+}
---
+++ WebApplicationGame/wwwroot/js/parseOBJ.js
@@ -0,0 +1,79 @@
+﻿function parseOBJ(path) {
+    var text = document.getElementById(path).outerText;
+
+    const objPositions = [[0, 0, 0]];
+    const objTexcoords = [[0, 0]];
+    const objNormals = [[0, 0, 0]];
+
+    const objVertexData = [
+        objPositions,
+        objTexcoords,
+        objNormals,
+    ];
+
+    let webglVertexData = [
+        [],   // positions
+        [],   // texcoords
+        [],   // normals
+    ];
+
+    function addVertex(vert) {
+        const ptn = vert.split('/');
+        ptn.forEach((objIndexStr, i) => {
+            if (!objIndexStr) {
+                return;
+            }
+            const objIndex = parseInt(objIndexStr);
+            const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+            webglVertexData[i].push(...objVertexData[i][index]);
+        });
+    }
+
+    const keywords = {
+        v(parts) {
+            objPositions.push(parts.map(parseFloat));
+        },
+        vn(parts) {
+            objNormals.push(parts.map(parseFloat));
+        },
+        vt(parts) {
+            objTexcoords.push(parts.map(parseFloat));
+        },
+        f(parts) {
+            const numTriangles = parts.length - 2;
+            for (let tri = 0; tri < numTriangles; ++tri) {
+                addVertex(parts[0]);
+                addVertex(parts[tri + 1]);
+                addVertex(parts[tri + 2]);
+            }
+        },
+    };
+
+    const keywordRE = /(\w*)(?: )*(.*)/;
+    const lines = text.split('\n');
+    for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+        const line = lines[lineNo].trim();
+        if (line === '' || line.startsWith('#')) {
+            continue;
+        }
+        const m = keywordRE.exec(line);
+        if (!m) {
+            continue;
+        }
+        const [, keyword, unparsedArgs] = m;
+        const parts = line.split(/\s+/).slice(1);
+        const handler = keywords[keyword];
+        if (!handler) {
+            continue;
+        }
+        handler(parts, unparsedArgs);
+    }
+
+    return {
+        position: webglVertexData[0],
+        texcoord: webglVertexData[1],
+        normal: webglVertexData[2],
+    };
+}
+
+
