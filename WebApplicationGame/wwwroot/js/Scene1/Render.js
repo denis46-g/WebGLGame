49c920fd091c08c5764c11fe78fd2297535d1ca1
leviathan-7 <m.grober02@yandex.ru>
2024-04-25 12:37:09 +0300

    update render

--- WebApplicationGame/wwwroot/js/Scene1/Render.js
+++ WebApplicationGame/wwwroot/js/Scene1/Render.js
@@ -17,31 +17,37 @@ function render() {
     var nMatrixs = [];
     //
     mvMatrixs[0] = mat4.create();
+    mat4.translate(mvMatrixs[0], mvMatrixs[0], [0, 0, 0]);
     mat4.scale(mvMatrixs[0], mvMatrixs[0], [0.1, 0.1, 0.1]);
     nMatrixs[0] = mat3.create();
     mat3.normalFromMat4(nMatrixs[0], mvMatrixs[0]);
     //
     mvMatrixs[1] = mat4.create();
+    mat4.translate(mvMatrixs[1], mvMatrixs[1], [0, 0, 0]);
     mat4.scale(mvMatrixs[1], mvMatrixs[1], [0.1, 0.1, 0.1]);
     nMatrixs[1] = mat3.create();
     mat3.normalFromMat4(nMatrixs[1], mvMatrixs[1]);
     //
     mvMatrixs[2] = mat4.create();
+    mat4.translate(mvMatrixs[2], mvMatrixs[2], [0, 0, 0]);
     mat4.scale(mvMatrixs[2], mvMatrixs[2], [0.1, 0.1, 0.1]);
     nMatrixs[2] = mat3.create();
     mat3.normalFromMat4(nMatrixs[2], mvMatrixs[2]);
     //
     mvMatrixs[3] = mat4.create();
+    mat4.translate(mvMatrixs[3], mvMatrixs[3], [0, 0, 0]);
     mat4.scale(mvMatrixs[3], mvMatrixs[3], [0.1, 0.1, 0.1]);
     nMatrixs[3] = mat3.create();
     mat3.normalFromMat4(nMatrixs[3], mvMatrixs[3]);
     //
     mvMatrixs[4] = mat4.create();
+    mat4.translate(mvMatrixs[4], mvMatrixs[4], [0, 0, 0]);
     mat4.scale(mvMatrixs[4], mvMatrixs[4], [0.1, 0.1, 0.1]);
     nMatrixs[4] = mat3.create();
     mat3.normalFromMat4(nMatrixs[4], mvMatrixs[4]);
     //
     mvMatrixs[5] = mat4.create();
+    mat4.translate(mvMatrixs[5], mvMatrixs[5], [0, 0, 0]);
     mat4.scale(mvMatrixs[5], mvMatrixs[5], [0.1, 0.1, 0.1]);
     nMatrixs[5] = mat3.create();
     mat3.normalFromMat4(nMatrixs[5], mvMatrixs[5]);
@@ -65,6 +71,9 @@ function render() {
     const texture1UniformLocation = gl.getUniformLocation(shaderProgram, "texture");
     gl.uniform1i(texture1UniformLocation, 0);
 
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.enableVertexAttribArray(textureAttributeLocation);
+
     for (var i = 0; i < 6; i++) {
         const texture = gl.createTexture();
         gl.activeTexture(gl.TEXTURE0 + 0);
@@ -75,10 +84,13 @@ function render() {
 
         gl.uniformMatrix4fv(mvMatrixLocation, false, mvMatrixs[i]);
         gl.uniformMatrix3fv(nMatrixLocation, false, nMatrixs[i]);
+
         gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffers[i]);
         gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
         gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffers[i]);
         gl.vertexAttribPointer(textureAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
         gl.drawElements(gl.TRIANGLES, objs[i].position.length - 2, gl.UNSIGNED_SHORT, 0);
     }
 
