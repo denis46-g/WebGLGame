294265cbc636e6b7b8437001abaa0bd140588dfb
leviathan-7 <m.grober02@yandex.ru>
2024-05-16 17:00:39 +0300

    optymize

--- WebApplicationGame/wwwroot/js/Scene1/Main_Script.js
+++ WebApplicationGame/wwwroot/js/Scene1/Main_Script.js
@@ -20,19 +20,13 @@ window.onload = function () {
         objs.push(parseOBJ(name));
     });
 
-    vertexPositionAttribute = gl.getAttribLocation(program, "aVertexPosition");
-    gl.enableVertexAttribArray(vertexPositionAttribute);
-    vertexTextureAttribute = gl.getAttribLocation(program, "aTextureCoord");
-    gl.enableVertexAttribArray(vertexTextureAttribute);
+    gl.enableVertexAttribArray(vertexPositionAttribute = gl.getAttribLocation(program, "aVertexPosition"));
+    gl.enableVertexAttribArray(vertexTextureAttribute = gl.getAttribLocation(program, "aTextureCoord"));
     gl.useProgram(program);
-
-    
-    
 
     squareVerticesBuffer = [];
     textureBuffer = [];
     indexBuffer = [];
-    texture = [];
 
     for (var i = 0; i < 5; i++) {
         gl.activeTexture(gl.TEXTURE0 + i);
@@ -43,31 +37,30 @@ window.onload = function () {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imgs[i]);
     }
     
-
-    render(imgs, objs, gl, program, lightLocation, x_location, z_location, true);
+    render(objs, gl, program, lightLocation, x_location, z_location, true);
 }
 
-function render(imgs, objs, gl, program, first) {
+function render(objs, gl, program, first) {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    draw_elem(imgs[0], objs[0], gl, program, 0, 0, 0, 1, first, 0, true);
-    draw_elem(imgs[1], objs[1], gl, program, x_location, 4 + z_location, 1, 0.07, first, 1, true);
-    draw_elem(imgs[2], objs[2], gl, program, teasurePoints[0][0], teasurePoints[0][1], 0, 1, first, 2, true);
-    draw_elem(imgs[2], objs[2], gl, program, teasurePoints[1][0], teasurePoints[1][1], 0, 1, false, 2, false);
-    draw_elem(imgs[2], objs[2], gl, program, teasurePoints[2][0], teasurePoints[2][1], 0, 1, false, 2, false);
-    draw_elem(imgs[3], objs[3], gl, program, scorpionPoints[0][0], scorpionPoints[0][1], 0, 0.5, first, 3, true);
-    draw_elem(imgs[3], objs[3], gl, program, scorpionPoints[1][0], scorpionPoints[1][1], 0, 0.3, false, 3, false);
-    draw_elem(imgs[4], objs[4], gl, program, xfish, 15, 1, 2, first, 4, true);
-    draw_elem(imgs[4], objs[4], gl, program, xfish - 7, 15, 1, 2, false, 4, false);
-    draw_elem(imgs[4], objs[4], gl, program, xfish + 7, 15, 1, 2, false, 4, false);
+    draw_elem(objs[0], gl, program, 0, 0, 0, 1, first, 0, true);
+    draw_elem(objs[1], gl, program, x_location, 4 + z_location, 1, 0.07, first, 1, true);
+    draw_elem(objs[2], gl, program, teasurePoints[0][0], teasurePoints[0][1], 0, 1, first, 2, true);
+    draw_elem(objs[2], gl, program, teasurePoints[1][0], teasurePoints[1][1], 0, 1, false, 2, false);
+    draw_elem(objs[2], gl, program, teasurePoints[2][0], teasurePoints[2][1], 0, 1, false, 2, false);
+    draw_elem(objs[3], gl, program, scorpionPoints[0][0], scorpionPoints[0][1], 0, 0.5, first, 3, true);
+    draw_elem(objs[3], gl, program, scorpionPoints[1][0], scorpionPoints[1][1], 0, 0.3, false, 3, false);
+    draw_elem(objs[4], gl, program, xfish, 15, 1, 2, first, 4, true);
+    draw_elem(objs[4], gl, program, xfish - 7, 15, 1, 2, false, 4, false);
+    draw_elem(objs[4], gl, program, xfish + 7, 15, 1, 2, false, 4, false);
 
     requestAnimationFrame(function () {
         document.getElementById("coord").textContent = "x = " + x_location.toFixed(4) + "; z = " + (-z_location - 4).toFixed(4) + ";";
-        render(imgs, objs, gl, program, false);
+        render(objs, gl, program, false);
         xfish-=0.1;
         if (xfish < -27)
             xfish = 27;
@@ -129,18 +122,15 @@ function nearObjPoints() {
         return true;
 }
 
-function draw_elem(img, obj, gl, program, x, z, y, k, first, i, first_in_item) {
+function draw_elem(obj, gl, program, x, z, y, k, first, i, first_in_item) {
     if (first) {
-        squareVerticesBuffer[i] = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer[i]);
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer[i] = gl.createBuffer());
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(obj.position.flat()), gl.STATIC_DRAW);
 
-        textureBuffer[i] = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer[i]);
+        gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer[i] = gl.createBuffer());
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(obj.texcoord.flat()), gl.STATIC_DRAW);
 
-        indexBuffer[i] = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer[i]);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer[i] = gl.createBuffer());
         let indices = [];
         for (let i = 0; i < obj.position.length; i++) {
             indices.push(i);
@@ -148,8 +138,7 @@ function draw_elem(img, obj, gl, program, x, z, y, k, first, i, first_in_item) {
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
     }
     if (first_in_item) {
-        const textureUniformLocation = gl.getUniformLocation(program, "texture");
-        gl.uniform1i(textureUniformLocation, i);
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), i);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer[i]);
         gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
@@ -164,26 +153,17 @@ function draw_elem(img, obj, gl, program, x, z, y, k, first, i, first_in_item) {
         0, 0, k, 0,
         0, 0, 0, 1];
     mat4.translate(mvMatrix, mvMatrix, [(1 / k) * (x - x_location), -(1.25 / k) + y, (1 / k) * (z_location - z)]);
-    var nMatrix = mat3.create();
-    mat3.normalFromMat4(nMatrix, mvMatrix);
-    var pMatrix = mat4.create();
-    mat4.perspective(pMatrix, 45 * Math.PI / 180, gl.canvas.height / gl.canvas.width, 0.01, 100.0);
+    mat3.normalFromMat4(nMatrix = mat3.create(), mvMatrix);
+    mat4.perspective(pMatrix = mat4.create(), 45 * Math.PI / 180, gl.canvas.height / gl.canvas.width, 0.01, 100.0);
 
-    const _AmbientLightColor = gl.getUniformLocation(program, "ambientLightColor");
-    gl.uniform3fv(_AmbientLightColor, AmbientLightColor);
-    const _DiffuseLightColor = gl.getUniformLocation(program, "diffuseLightColor");
-    gl.uniform3fv(_DiffuseLightColor, DiffuseLightColor);
-    const _SpecularLightColor = gl.getUniformLocation(program, "specularLightColor");
-    gl.uniform3fv(_SpecularLightColor, SpecularLightColor);
-    const _uLightPosition = gl.getUniformLocation(program, "lightLocation");
-    gl.uniform3fv(_uLightPosition, lightLocation);
+    gl.uniform3fv(gl.getUniformLocation(program, "ambientLightColor"), AmbientLightColor);
+    gl.uniform3fv(gl.getUniformLocation(program, "diffuseLightColor"), DiffuseLightColor);
+    gl.uniform3fv(gl.getUniformLocation(program, "specularLightColor"), SpecularLightColor);
+    gl.uniform3fv(gl.getUniformLocation(program, "lightLocation"), lightLocation);
 
-    const mvMatUni = gl.getUniformLocation(program, "mvMatrix");
-    gl.uniformMatrix4fv(mvMatUni, false, mvMatrix);
-    const nMatUni = gl.getUniformLocation(program, "nMatrix");
-    gl.uniformMatrix3fv(nMatUni, false, nMatrix);
-    const pMatUni = gl.getUniformLocation(program, "pMatrix");
-    gl.uniformMatrix4fv(pMatUni, false, pMatrix);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "mvMatrix"), false, mvMatrix);
+    gl.uniformMatrix3fv(gl.getUniformLocation(program, "nMatrix"), false, nMatrix);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "pMatrix"), false, pMatrix);
 
     gl.drawElements(gl.TRIANGLES, obj.position.length - 2, gl.UNSIGNED_SHORT, 0);
 }
