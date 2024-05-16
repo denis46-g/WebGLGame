ec96187357df2511191bf5b21b81dc4d41c4a803
leviathan-7 <m.grober02@yandex.ru>
2024-05-16 22:12:33 +0300

    update vsShaders

--- WebApplicationGame/wwwroot/js/Scene1/vsShaders.js
+++ WebApplicationGame/wwwroot/js/Scene1/vsShaders.js
@@ -15,11 +15,12 @@
     varying vec3 v_LightWeighting;
     varying vec2 v_aTextureCoord;
 
-    void main(void) {
+    vec3 phong(vec3 loc)
+    {
         float shininess = 0.5;
         vec4 vertexPositionEye4 = mvMatrix * vec4(aVertexPosition, 1.0);
         vec3 vertexPositionEye3 = vertexPositionEye4.xyz / vertexPositionEye4.w;
-        vec3 lightDirection = normalize(lightLocation - vertexPositionEye3);
+        vec3 lightDirection = normalize(loc - vertexPositionEye3);
         vec3 normal = normalize(nMatrix * aVertexPosition.xyz);
         float lightDist = length(lightDirection);
         float lightIntens = 1.0 / (1.0 + 0.1 * lightDist + 0.01 * lightDist * lightDist);
@@ -28,10 +29,13 @@
         vec3 viewVectorEye = -normalize(vertexPositionEye3);
         float specularLightDot = max(dot(reflectionVector, viewVectorEye), 0.0);
         float specularLightParam = pow(specularLightDot, shininess);
+        return (diffuseLightColor * diffuseLightDot + specularLightColor * specularLightParam) * lightIntens;
+    }
 
+    void main(void) {
         v_LightWeighting = ambientLightColor;
         if(lightingSpace){
-            v_LightWeighting += (diffuseLightColor * diffuseLightDot + specularLightColor * specularLightParam) * lightIntens;
+            v_LightWeighting += phong(lightLocation);
         }
         v_aTextureCoord = aTextureCoord;
         gl_Position = pMatrix * mvMatrix * vec4(aVertexPosition, 1.0);
