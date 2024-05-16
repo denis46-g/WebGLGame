6a2d94809eba1125a7fef88d2aec5f7a501efd3f
leviathan-7 <m.grober02@yandex.ru>
2024-05-16 12:39:57 +0300

    add y angle

--- WebApplicationGame/wwwroot/js/Scene1/Main_Script.js
+++ WebApplicationGame/wwwroot/js/Scene1/Main_Script.js
@@ -140,6 +140,7 @@ function draw_elem(img, obj, gl, program, x, z, y, k, first, i, first_in_item) {
         gl.bindTexture(gl.TEXTURE_2D, texture[i]);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     }
     if (first_in_item) {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
