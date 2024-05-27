63da8e2f1f157431bc2da34b2942b46ce98ffb2f
EternalDebug <gurin.ivan.2001@mail.ru>
2024-05-27 23:45:02 +0300

    Second scene added

--- WebApplicationGame/wwwroot/js/Scene2/fsShaders.js
+++ WebApplicationGame/wwwroot/js/Scene2/fsShaders.js
@@ -8,13 +8,21 @@
     uniform mat4 mvMatrix2;
     uniform mat3 nMatrix;
     uniform vec3 lightLocation;
+    uniform vec3 lightLocation2;
     uniform vec3 ambientLightColor;
     uniform vec3 diffuseLightColor;
     uniform vec3 specularLightColor;
     uniform bool lightingSpace;
+    uniform bool lightingSpace2;
+
+    uniform float amb;
+    uniform float att;
+    uniform float attL;
+
     varying vec2 v_aTextureCoord;
     varying vec3 v_aVertexPosition;
     varying vec3 v_aNormal;
+
 
     vec3 phong(vec3 loc)
     {
@@ -22,6 +30,10 @@
         vec4 vertexPositionEye4 = mvMatrix * vec4(v_aVertexPosition, 1.0);
         vec3 vertexPositionEye3 = vertexPositionEye4.xyz / vertexPositionEye4.w;
         vec3 lightDirection = normalize(loc - vertexPositionEye3);
+
+        float distanceToLight = length(lightDirection);
+        float attenuation = 1.0 / (att + attL * distanceToLight);
+
         vec3 normal = normalize(nMatrix * v_aNormal.xyz);
         float lightDist = length(lightDirection);
         float lightIntens = 1.0 / (1.0 + 0.1 * lightDist + 0.01 * lightDist * lightDist);
@@ -30,14 +42,16 @@
         vec3 viewVectorEye = -normalize(vertexPositionEye3);
         float specularLightDot = max(dot(reflectionVector, viewVectorEye), 0.0);
         float specularLightParam = pow(specularLightDot, shininess);
-        return (diffuseLightColor * diffuseLightDot + specularLightColor * specularLightParam) * lightIntens;
+        return (diffuseLightColor * diffuseLightDot + specularLightColor * specularLightParam) * lightIntens * attenuation;
     }
 
     void main(void) {
         vec4 pixel = texture2D(texture, v_aTextureCoord);
-        vec3 lightLocation2 = (mvMatrix2 * vec4(lightLocation, 1.0)).xyz;
-        vec3 lightWeighting = ambientLightColor;
+        vec3 lightWeighting = ambientLightColor * amb;
         if(lightingSpace){
+            lightWeighting += phong(lightLocation);
+        }
+        if(lightingSpace2){
             lightWeighting += phong(lightLocation2);
         }
 
